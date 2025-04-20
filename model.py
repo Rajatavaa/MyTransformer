14c1f84bd7abf3ee9c01ba248fac8a04775e628f
Rajatava Ghosh <ghoshrick10@gmail.com>
2025-04-20 19:31:55 +0530

    first commit

---
+++ model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import math 
+
+class Inputembeddings(nn.Module):
+    def __init__(self,d_model:int,vocab_size:int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self,d_model:int,seq_length:int,dropout:float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_length = seq_length
+        self.dropout = nn.Dropout(dropout)
+        
+        pe = torch.zeros(seq_length, d_model)
+        pos = torch.arange(0, seq_length, dtype=torch.float).unsqueeze(1) #adding a dimension along column
+        #arrange is used to create a sequence of numbers
+        div = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+        pe[:,0::2] = torch.sin(pos*div)
+        pe[:,1::2] = torch.cos(pos*div)
+        pe = pe.unsqueeze(0)
+        self.resgister_buffer('pe', pe)
+        
+        def forward(self,x):
+            x = x + (self.pe[:,:x.shape[1],:]).require_grad_(False)
+            return self.dropout(x)
