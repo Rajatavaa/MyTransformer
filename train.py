00a481c81320068925e00efce6cc9c0644a56a3e
Rajatava Ghosh <ghoshrick10@gmail.com>
2025-11-04 01:03:10 +0530

    Added tokenizer training

---
+++ train.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+from datasets import Dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.trainers import WordLevelTrainer
+from pathlib import Path
+
+def get_tokenizer_load(config,ds,lang):
+    tokenizer_path = Path(config["tokenizer.json"].format(lang))
+    if not Path.exists(tokenizer_path):
+        # WordLevel requires a vocab dict; provide an empty dict to be populated by the trainer
+        tokenizer = Tokenizer(WordLevel({}, unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()  # type: ignore
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"],min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds,lang),trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer    
+def get_all_sentences(Dataset):
+    
