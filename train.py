cad9f52f8726496295eb241d2cd36c0906d5864a
Rajatava Ghosh <ghoshrick10@gmail.com>
2025-11-12 01:12:10 +0530

    Added torch.compile

--- train.py
+++ train.py
@@ -191,6 +191,7 @@ def train_model(config):
     
     train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(config)
     model = get_model(config,tokenizer_src.get_vocab_size(),tokenizer_tgt.get_vocab_size()).to(device)
+    torch.compile(model)
     #Tensorboard
     writer = SummaryWriter(config['experiment_name'])
     optimizer = torch.optim.Adam(model.parameters(),lr=config['lr'],eps = 1e-9)
